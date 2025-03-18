53a56d7afd97461024dee24b4f9bc952db086d33
Shohei Fujita <131703944+shoheifujitaSF@users.noreply.github.com>
2025-03-18 11:50:02 -0400

    Delete reconstruction directory

--- reconstruction/rmse.m
+++
@@ -1,6 +0,0 @@
-function [ res ] = rmse( img1, img2 )
-
-res = norm(img1(:)-img2(:)) / norm(img2(:));
-
-end
-
--- reconstruction/rsos.m
+++
@@ -1,5 +0,0 @@
-function [img_rsos] = rsos( img, dim )
-
-img_rsos = squeeze(sqrt(sum(img.*conj(img),dim)));
-
-end
